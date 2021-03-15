56532ab97defaf27cb55a1b76ae981ee2e87785c
koodilauri <lakoivisto@gmail.com>
2021-03-15 17:15:30 +0000

    Deploying to gh-pages from @ koodilauri/koodilauri.github.io@80c5cd2b70eaf77d248885a02a0cea3d0023b5fe 🚀

--- _next/static/BbrtMQbBkbR5xuseHPK9o/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F9c4381274c2a4fd9d205.css","static\u002Fchunks\u002Fpages\u002Findex-28529a71d7d02073c4ec.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ecdaf96375158042f3e7.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/vSxXyrCkAzoiRKvdhDgr7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F9c4381274c2a4fd9d205.css","static\u002Fchunks\u002Fpages\u002Findex-bb516639e546ff710b5d.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ecdaf96375158042f3e7.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
