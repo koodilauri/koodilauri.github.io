983c75c459a0131122d0f0e7914a65f74bc18588
Lauri Koivisto <lakoivisto@gmail.com>
2021-03-30 18:47:12 +0300

    Add tailwindcss-jit

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/jit': {},
     autoprefixer: {},
-    'postcss-import': {}
   },
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,10 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    backgroundColor: theme => ({
-      ...theme('colors'),
-      'primary': '#4c1d95',
-      'secondary': '#ffed4a',
-      'danger': '#e3342f',
-      'shade': '#08030f'
-    }),
     extend: {
       fontSize: {
         '6xl': '4rem'
