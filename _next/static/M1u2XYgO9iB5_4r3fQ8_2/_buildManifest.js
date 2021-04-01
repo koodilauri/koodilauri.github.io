850aabeae586983870435bba1058273994f2090c
koodilauri <lakoivisto@gmail.com>
2021-04-01 13:56:03 +0000

    Deploying to gh-pages from @ koodilauri/koodilauri.github.io@88a1b2b22ee2a75012252294f2cb9fae7b867054 🚀

--- _next/static/M1u2XYgO9iB5_4r3fQ8_2/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-8747f72315a0c0d6f554.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-41da47b524ec76619a9a.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
